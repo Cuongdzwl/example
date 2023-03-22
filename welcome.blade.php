2a23106467be5019ce712807f7b7a2cf7fe7cde4
vutrn <121750583+vutrn@users.noreply.github.com>
2023-03-22 16:30:11 +0700

    webcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+
+</head>
+
+<body>
+  <h1 class="font-bold text-4xl underline px-5 flex 
+             transition: 1s hover:bg-red-100">
+    Hello world
+  </h1>
+</body>
+</html>
